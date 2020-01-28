6b9fab348f0e71e59f177b5b094e9c7220847eb2
stla <laurent_step@yahoo.fr>
2020-01-28 12:57:31 +0100

    improve .EllipseFromCenterAndEigen

--- R/internal.R
+++ R/internal.R
@@ -153,7 +153,7 @@
 .EllipseFromCenterAndEigen <- function(center, e){
   v <- e$vectors[,2L]
   alpha <- (atan2(v[2L],v[1L]) * 180/pi) %% 180
-  a <- 1/sqrt(e$values[2L])#.vlength(v/r)
-  b <- a * sqrt(e$values[2L]/e$values[1L])
+  a <- 1/sqrt(e$values[2L])
+  b <- 1/sqrt(e$values[1L])
   Ellipse$new(center, a, b, alpha)
 }
